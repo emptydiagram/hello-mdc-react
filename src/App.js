e11b27f6da7d011347d3d1caaab5157791e5f974
maniacalhonks <maniacalhonks@gmail.com>
2019-10-27 17:10:29 -0400

    Add @material/react-text-field

--- src/App.js
+++ src/App.js
@@ -4,45 +4,45 @@ import TextField, {HelperText, Input} from '@material/react-text-field';
 
 import './App.css';
 import '@material/react-button/dist/button.css';
+import '@material/react-text-field/dist/text-field.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.setState({
-      name: '',
-      email: '',
-    });
+    this.state = {name: '', email: ''};
   }
 
-  return (
-    <div className="App">
-      <main className="App-main">
-        <h2>:P testing form components</h2>
-        <Button
-          raised
-          className='button-alternate'
-          onClick={() => console.log('clicked!')}
-        >
-          Click Me!
-        </Button>
-        <p>
-          <TextField label='Name'>
-            <Input
-              value={this.state.name}
-              onChange={(e) => this.setState({name: e.currentTarget.value})} />
-          </TextField>
-        </p>
-        <p>
-          <TextField label='Email Address'>
-            <Input
-              value={this.state.email}
-              onChange={(e) => this.setState({email: e.currentTarget.value})} />
-          </TextField>
-        </p>
-      </main>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <main className="App-main">
+          <h2>:P testing form components</h2>
+          <Button
+            raised
+            className='button-alternate'
+            onClick={() => console.log("clicked! name:" + this.state.name + ", email: " + this.state.email)}
+          >
+            Click Me!
+          </Button>
+          <p>
+            <TextField label='Name'>
+              <Input
+                value={this.state.name}
+                onChange={(e) => this.setState({name: e.currentTarget.value})} />
+            </TextField>
+          </p>
+          <p>
+            <TextField label='Email Address'>
+              <Input
+                value={this.state.email}
+                onChange={(e) => this.setState({email: e.currentTarget.value})} />
+            </TextField>
+          </p>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
