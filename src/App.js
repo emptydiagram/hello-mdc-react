330762716d27a60f75414a60ad0e36c366f2d7d2
maniacalhonks <maniacalhonks@gmail.com>
2019-10-27 17:52:56 -0400

    Add @material/react-tab-bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Button from '@material/react-button';
+import Tab from '@material/react-tab';
+import TabBar from '@material/react-tab-bar';
 import TextField, {HelperText, Input} from '@material/react-text-field';
 
 import './App.css';
 import '@material/react-button/dist/button.css';
+import '@material/react-tab/dist/tab.css';
+import '@material/react-tab-bar/dist/tab-bar.css';
+import '@material/react-tab-scroller/dist/tab-scroller.css';
 import '@material/react-text-field/dist/text-field.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {name: '', email: ''};
+    this.state = {name: '', email: '', activeIndex: 2};
   }
 
   render() {
@@ -35,6 +40,22 @@ class App extends React.Component {
               value={this.state.email}
               onChange={(e) => this.setState({email: e.currentTarget.value})} />
           </TextField>
+          <div>
+            <TabBar
+              activeIndex={this.state.activeIndex}
+              handleActiveIndexUpdate={this.handleActiveIndexUpdate}
+            >
+              <Tab>
+                <span className='mdc-tab__text-label'>One</span>
+              </Tab>
+              <Tab>
+                <span className='mdc-tab__text-label'>Two</span>
+              </Tab>
+              <Tab>
+                <span className='mdc-tab__text-label'>Three</span>
+              </Tab>
+            </TabBar>
+          </div>
         </main>
       </div>
     );
